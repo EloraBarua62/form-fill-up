20cc209430b71cd779f6174bb5de7dfa90ac4fe5
EloraBarua <elorabarua62@gmail.com>
2023-07-25 21:33:53 +0600

    project initialized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+// Imported files
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+      {/* Form */}
+      <h1 className='text-2xl font-bold text-green-500'>Form fillup</h1>
     </div>
   );
 }
