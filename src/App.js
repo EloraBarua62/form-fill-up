9fc903237e3cbc28ddf594ba9d8562d854af52ac
EloraBarua <elorabarua62@gmail.com>
2023-07-26 21:32:49 +0600

    Category initialized and Add new category option created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 // Imported files
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [category , setCategory] = useState([]);
+
+  const handleAdd = () => {
+    const previous = [...category , []];
+    setCategory(previous);
+  }
   return (
     <div className="App">
       
-
+      
       {/* Form */}
-      <h1 className='text-2xl font-bold text-green-500'>Form fillup</h1>
+      <h1 className="text-2xl font-bold text-green-500">Form fillup</h1>
+
+      {category.map((data , index) => {
+        return (
+          <input className='bg-green-100 border-[1px] border-green-500' onChange={ e => e.handleNewCategory(e,index)} type="text" />
+        )
+
+      })}
+
+    {/* Add new category */}
+      <button onClick={() => handleAdd()} className='bg-green-500'>Add New Category</button>
     </div>
   );
 }
